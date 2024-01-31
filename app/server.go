6b617013e9846844cc500f68cebd4009b38d6aaf
Mikhail Pikin <mikejke99@gmail.com>
2024-01-31 22:15:15 +0300

    pass 5th stage

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,6 +15,7 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	defer l.Close()
 
 	for {
 		conn, err := l.Accept()
@@ -21,25 +23,49 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		defer conn.Close()
-		go handleClient(conn)
+
+		go handleConnection(conn)
 	}
 }
 
-func handleClient(conn net.Conn) {
-	for {
-		buf := make([]byte, 1024)
-		n, err := conn.Read(buf)
-		if err != nil {
-			fmt.Println("Error while reading: ", err.Error())
-			continue
-		}
+const (
+	PING = "PING"
+	ECHO = "ECHO"
+)
 
-		fmt.Printf("Received message from %s: %s", conn.RemoteAddr(), string(buf[:n]))
-		if _, err := conn.Write([]byte("+PONG\r\n")); err != nil {
-			fmt.Println("Error while responding: ", err.Error())
+func handleConnection(conn net.Conn) {
+	client, err := NewClient(conn)
+	if err != nil {
+		fmt.Println("Error: ", err.Error())
+		return
+	}
+	defer client.Close()
+
+	for {
+		result, err := client.Read()
+		if err != nil {
+			fmt.Println("Error: ", err.Error())
 			return
 		}
-	}
 
+		content := result.Content()
+
+		switch c := content.(type) {
+		// case string:
+		// 	if c == PING {
+		// 		client.conn.Write([]byte("+PONG\r\n"))
+		// 	}
+		case []interface{}:
+			if c[0] == ECHO {
+				var message string
+				for i := 1; i < len(c); i++ {
+					message += fmt.Sprintf("%v ", c[i])
+				}
+				client.conn.Write([]byte(strings.TrimRight(message, " ")))
+			}
+		default:
+			fmt.Println("unknown command")
+			client.conn.Write([]byte("+PONG\r\n"))
+		}
+	}
 }
